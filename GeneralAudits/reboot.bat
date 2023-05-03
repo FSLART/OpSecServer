41d0cdb830f038dec2c5add51e449208dd2fb791
Gameiro <alexandrefgame@gmail.com>
2023-05-03 16:27:02 +0100

    General Audit OR syntax fix

--- GeneralAudits/reboot.bat
+++ GeneralAudits/reboot.bat
@@ -3,16 +3,22 @@
 ::Check the arguments 
 set "reversed=/rd:true"
 set "format=/f:xml"
-if "%1"=="-h" || "%1"=="-help" || "%1"=="--help" || "%1"=="/h" || "%1"=="-?" || "%1"== "ajuda" goto :help
+if "%1"=="-h" goto help
+if "%1"=="-help" goto help
+if "%1"=="--help" goto help
+if "%1"=="/h" goto help
+if "%1"=="-?" goto help
+if "%1"== "ajuda" goto help
 
 if "%1"=="-r" set "reversed=/rd:%2"
 if "%1"=="-f" set "format=/f:%2"
+goto noarg
 ::help
 :help
 echo "Reboot Audit Scripts:\nEste comando e usado para teres uma lista de todos os reboots que occoreram no sistema\n So tem dois parametros ambos estes nao sao obrigatorios: \n\n -r <flag> : que dita a ordem chronologica com que os registos sao colocados(por defeito o valor e true ou seja os mais recentes sao escritos primeiro\n -f <formato> : Este parametro permite definir o tipo de formato em que o output e mostrado(por defeito o valor e xml)\n\nNOTA IMPORTANTE: devido a natureza deste comando o valor de formato deve ser deixado a default pois e mais optimizado se mesmo assim precisares doutro formato ve a documentaçao do wevtutil"
 echo "\nNOTA IMPORTANTE2: NAO EXISTE VALIDAÇAO DE PARAMETROS, ie se fuderes o comando too bad (e nao te queixes)->gerado por AI"
 echo "\n\@fsipleiria 2023, (o joaoV teve aq)"
-goto :end
+goto end
 ::noarg
 :noarg
 :: wevtutil qe System /rd:true with the following list of events 41 1074 1076 6005 6006 6008 6009
